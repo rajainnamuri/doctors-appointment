d01eb880837cd788c2eea313436427a7c6e1b9df
devaharsha <devaharsha@gmail.com>
2024-09-18 19:55:41 +0530

    yep

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
-
+import React from 'react';
+import './Contact.css';
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section>
+      <div className="max-w-screen-md px-4 mx-auto">
+        <h2 className="mb-8 text-center">Contact Us</h2>
+        <p className="mb-8 font-light text-center lg:mb-16 text_para">
+          If you have any technical issues send feed back?Let us know.
+        </p>
+        <form action='#' className="space-y-8">
+          <div>
+            <label htmlFor="email" className="form_label">
+              Enter your mail id
+            </label>
+            <input type="email" id="email" placeholder="abcd@gmail.com"
+            className="mt-1 form_input"/>
+          </div>
+
+        </form>
+      </div>
+    </section>
   )
 }
 
--- frontend/src/pages/Doctors/SidePanel.jsx
+++ frontend/src/pages/Doctors/SidePanel.jsx
@@ -1,36 +1,35 @@
-import React from 'react'
+import React from 'react';
+import './SidePanel.css';
 
 const SidePanel = () => {
   return (
-    <div className='p-3 rounded shadow-panelShadow lg:p-5-md'>
+    <div className="p-3 rounded shadow-panelShadow lg:p-5-md side-panel">
       <div className="flex items-center justify-between">
-        <p className='mt-0 font-semibold text_para'>Ticket price</p>
-        <span className="text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor  font-bold">500 BDT</span>
+        <p className="mt-0 font-semibold text_para">Ticket price</p>
+        <span className="text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor font-bold">
+          500 BDT
+        </span>
       </div>
       <div className="mt-[30px]">
         <p className="mt-0 font-semibold text_para text-headingColor">Available Time Slots:</p>
-
         <ul className="mt-3">
           <li className="flex items-center justify-between mb-2">
-            <p className='text-[15px] leading-6 text-textColor font-semibold'>Sunday</p>
-            <p className='text-[15px] leading-6 text-textColor font-semibold'>4:00 PM - 9:30 PM</p>
+            <p className="text-[15px] leading-6 text-textColor font-semibold">Sunday</p>
+            <p className="text-[15px] leading-6 text-textColor font-semibold">4:00 PM - 9:30 PM</p>
           </li>
-          
           <li className="flex items-center justify-between mb-2">
-            <p className='text-[15px] leading-6 text-textColor font-semibold'>Monday</p>
-            <p className='text-[15px] leading-6 text-textColor font-semibold'>4:00 PM - 9:30 PM</p>
+            <p className="text-[15px] leading-6 text-textColor font-semibold">Monday</p>
+            <p className="text-[15px] leading-6 text-textColor font-semibold">4:00 PM - 9:30 PM</p>
           </li>
-
           <li className="flex items-center justify-between mb-2">
-            <p className='text-[15px] leading-6 text-textColor font-semibold'>Tuesday</p>
-            <p className='text-[15px] leading-6 text-textColor font-semibold'>4:00 PM - 9:30 PM</p>
+            <p className="text-[15px] leading-6 text-textColor font-semibold">Tuesday</p>
+            <p className="text-[15px] leading-6 text-textColor font-semibold">4:00 PM - 9:30 PM</p>
           </li>
         </ul>
       </div>
-      <button className='w-full px-2 rounded btn-md'>Book Appointment</button>
-      
+      <button className="w-full px-2 rounded btn-md">Book Appointment</button>
     </div>
-  )
-}
+  );
+};
 
-export default SidePanel
+export default SidePanel;
